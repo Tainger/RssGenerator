6db678a54fde2d019a86429cb4a5ccbeb5624091
rocky <1131819936@qq.com>
2023-07-30 14:33:17 +0800

    完成asf jira的rss生成 .ignore todo: jseesionid 刷新

---
+++ src/main/java/com/knowns/rss/generator/controller/github/GithubController.java
@@ -0,0 +1,12 @@
+package com.knowns.rss.generator.controller.github;
+
+/**
+ * @author jiazhiyuan
+ * @date 2023/7/30 1:51 PM
+ */
+public class GithubController {
+}
+
+
+
+    
--- src/main/java/com/knowns/rss/generator/dao/JdItemDao.java
+++
@@ -1,8 +0,0 @@
-package com.knowns.rss.generator.dao;
-
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface JdItemDao {
-}
